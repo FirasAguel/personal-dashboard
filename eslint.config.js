7a0e2f2b30bfbdf7c037ba5aebd4024de216114d
Mohamed Firas Aguel <firas.aguel@gmail.com>
2025-01-19 21:19:25 +0900

    chores
    eslint
    prettier
    pnpm install
    pnpm upgrade

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,14 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import pluginReactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
-
+import pluginReactJSXRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     settings: {
       react: {
-        version: "19.0.0",
+        version: '19.0.0',
       },
     },
     languageOptions: {
@@ -30,7 +29,7 @@ export default [
   },
   {
     ignores: [
-      '.next',      // Ignore .next directory
+      '.next', // Ignore .next directory
       'node_modules', // Ignore node_modules explicitly (optional)
     ],
   },
