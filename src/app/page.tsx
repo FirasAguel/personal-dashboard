db17b136fb43e2d47dfcc66fcbd154f46f9f150f
Mohamed Firas Aguel <firas.aguel@gmail.com>
2025-01-19 17:33:55 +0900

    add add city functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,55 @@
 "use client"
+import React, { useState, useEffect } from "react";
 import WeatherWidget from "@/components/WeatherWidget";
+import Modal from "@/components/Modal";
+import { cityCoordinates } from "../../config/cityCoordinates";
+
+const cities = Object.keys(cityCoordinates).sort();
 
 export default function Home() {
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [widgets, setWidgets] = useState<string[]>(["Gabes", "Tokyo", "Paris", "Atlantis"]);
+  const [selectedCity, setSelectedCity] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCity(event.target.value);
+  };
+  
+  
   return (
     <>
+    <Modal 
+        onClose={() => setIsModalOpen(false)} 
+        visible={isModalOpen}
+      >
+        <h2>Select a City</h2>
+        <div style={{display:"flex"}}>
+          <select onChange={handleChange}>
+            {cities.map((city,i)=> (<option value={city} key={i}>{city}</option>))}
+            <option value="Add new city">Add new city</option>
+          </select>
+          <button onClick={ () => {
+              if(selectedCity === "Add new city" ){
+                console.log("Handle add new city");
+              }
+              else if (widgets.includes(selectedCity)) {
+                console.log("City widget already displayed");
+              }
+              else{
+                setWidgets((prev) => [...prev, selectedCity]);
+                setIsModalOpen(false);
+              }
+            } }>
+            Add
+          </button>
+        </div>
+      </Modal>
+    
     <div className="dashboard">
-      <WeatherWidget city="Gabes" />
-      <WeatherWidget city="Tokyo" />
-      <WeatherWidget city="Paris" />
-      <WeatherWidget city="Atlantis" />
-      <div className="add-widget widget">
+      {widgets.map( (city)=> (<WeatherWidget city={city} key={city}/>))}
+      <div className="add-widget widget" onClick={() => setIsModalOpen(true)}>
         + Add city
       </div>
     </div>
