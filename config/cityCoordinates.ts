94260dccc4eadda134bd96a3205e8da21312076c
Mohamed Firas Aguel <firas.aguel@gmail.com>
2025-01-19 11:50:34 +0900

    optimize fetchCoordinates
    added cities to a config file for scalability

---
+++ config/cityCoordinates.ts
@@ -0,0 +1,11 @@
+export const cityCoordinates: Record<string, { latitude: number; longitude: number }> = {
+    "Gabes": { latitude: 33.8833, longitude:  10.1167 },
+    "Tokyo": { latitude: 35.6895, longitude: 139.6917 },
+    "Hino" : { latitude: 35.6713, longitude: 139.3951 },
+    "Paris" : { latitude: 48.8567, longitude: 2.3522 },
+    "Porto": { latitude: 41.1621, longitude: -8.622 },
+    "London": { latitude: 51.5072, longitude: -0.1275 },
+    "Le Mans": { latitude: 48.0077, longitude: 0.1984 },
+    "Essen": { latitude: 51.4508, longitude: 7.0131 },
+    "New York": { latitude: 40.7128, longitude: -74.006 },
+};
